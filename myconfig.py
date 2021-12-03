7e90872a8f65b6784b0cfff7660ecaca1971eb6c
DragonDreamer <luochong@whu.edu.cn>
2021-12-03 15:49:27 +0800

    main
    super generator!

---
+++ myconfig.py
@@ -0,0 +1,12 @@
+out_dir = "."
+group_id = "com.github.blog"
+name = "myblog"
+mysql = {
+    "url": "10.254.4.24",
+    "database": "myblog",
+    "username": "root",
+    "password": "123456",
+    "tableNames": ["cms_article", "cms_comment", "cms_leave_message"],
+    "idTypes": ["Integer", "Long", "Integer"]
+}
+temp_group_id = "com.macro.mall"
